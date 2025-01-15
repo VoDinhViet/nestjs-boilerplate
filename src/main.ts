2d282105ca443af5c9d13526fbb21adce9281af0
VoDinhViet <vietvd@DESKTOP-TTRE8RC.>
2025-01-15 06:25:00 +0000

    feat: add and config swagger ui

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,15 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { AllConfigType } from './config/config.type';
+import { Environment } from './constants/app.constant';
+import setupSwagger from './utils/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<AllConfigType>);
+  const isProduction =
+    configService.get('app.nodeEnv', { infer: true }) ===
+    Environment.PRODUCTION;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -24,13 +29,20 @@ async function bootstrap() {
     }),
   );
 
+  //************************************************************
+  // [Enable/Disable] Swagger UI
+  //************************************************************
+  if (!isProduction) {
+    setupSwagger(app);
+  }
+
   await app.listen(
     configService.getOrThrow('app.port', { infer: true }),
     async () => {
       console.info(`
     ======================================================================================================
         Name: [${configService.getOrThrow('app.name', { infer: true })}] - Port: [${configService.getOrThrow('app.port', { infer: true })}] - Environment: [${configService.getOrThrow('app.nodeEnv', { infer: true })}]
-        Swagger UI: ${(await app.getUrl()).replace(`[::1]`, `localhost`).trim()}/api-docs
+        ${!isProduction ? `Swagger UI: ${(await app.getUrl()).replace(`[::1]`, `localhost`).trim()}/api-docs` : 'Swagger UI: Disabled'}
     ======================================================================================================
   `);
     },
